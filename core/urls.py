4c809d85e5088b0b97bb5117184d4810471ea2ca
HishamHikami <hishamhikami@outlook.com>
2024-05-29 18:10:49 +0530

    Second commit

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import blog_detail, blog_list, tag_list, index, ajax_contact_form, ajax_get_quote
+from .views import blog_detail, blog_list, tag_list, index, ajax_contact_form, ajax_get_quote, category_list
 from django.contrib.sitemaps.views import sitemap
 from .sitemap import BlogSitemap
 
-app_name = "blog"
+app_name = "core"
 
 sitemaps = {
     'posts': BlogSitemap
@@ -14,6 +14,7 @@ urlpatterns = [
     path("blog/", blog_list, name='blog'),
     path("blog/<slug>", blog_detail, name='blog-detail'),
     path("blog/tag/<tag_slug>/", tag_list, name='tags'),
+    path("blog/category/<category_slug>/", category_list, name='categories'),
     path(
         "blog-sitemap.xml",
         sitemap,
--- core/views.py
+++ core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, render
-from .models import Post, Contact, GetQuote
+from .models import Post, Contact, GetQuote, Category
 from taggit.models import Tag
 
 # Create your views here.
@@ -18,15 +18,27 @@ def index(request):
     return render(request, 'core/index.html', context)
 
 def blog_detail(request, slug):
-    post = Post.objects.get(slug=slug)
+    post = get_object_or_404(Post, slug=slug)
     posts = Post.objects.filter(status="published")
+    categories = Category.objects.all()
+    tags = post.tags.all()
+    
+    category = None
+    if post.category:
+        try:
+            category = Category.objects.get(slug=post.category.slug)
+        except Category.DoesNotExist:
+            pass
 
     context = {
         "post": post,
         "posts": posts,
+        "category": category,
+        "categories": categories,
+        "tags": tags,
     }
     
-    return render(request, 'blog/blog-detail.html', context)
+    return render(request, 'core/blog-detail.html', context)
 
 def blog_list(request):
     posts = Post.objects.filter(status="published").order_by("-date")
@@ -37,7 +49,7 @@ def blog_list(request):
         "f_posts": f_posts,
     }
 
-    return render(request, 'blog/blog-list.html', context)
+    return render(request, 'core/blog-list.html', context)
 
 def tag_list(request, tag_slug=None):
     posts = Post.objects.filter(status="published").order_by("-date")
@@ -52,7 +64,22 @@ def tag_list(request, tag_slug=None):
         "tag": tag,
     }
 
-    return render(request, 'blog/tag.html', context)
+    return render(request, 'core/tag.html', context)
+
+def category_list(request, category_slug=None):
+    posts = Post.objects.filter(status="published").order_by("-date")
+
+    tag = None
+    if category_slug:
+        category = get_object_or_404(Tag, slug=category_slug)
+        posts = posts.filter(category__in=[category])
+
+    context = {
+        "posts": posts,
+        "tag": tag,
+    }
+
+    return render(request, 'core/category.html', context)
 
 def ajax_contact_form(request):
     full_name = request.GET['full_name']
