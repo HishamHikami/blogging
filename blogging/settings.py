f09e6c70d9b1535fe61e1480b22b6bea9deec4dc
Hisham Hikami <hishamhikami@outlook.com>
2024-05-31 20:03:21 +0530

    preparing for deploy

--- blogging/settings.py
+++ blogging/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DEBUG', False))
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '64.23.231.113']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '64.23.231.113', 'auxnid.com', 'www.auxnid.com']
 
 SITE_ID = 1
 
